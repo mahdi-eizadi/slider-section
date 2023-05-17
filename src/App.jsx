84e9a8fdd439662ee0a002c5d9201546b3421a69
mahdi-eizadi <mahdi.eizadi98@gmail.comgit config --global user.name>
2023-05-17 19:14:00 +0330

    upload Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Carousel from './Carousel';
+import SlickCarousal from './SlickCarousal';
+
+const App = () => {
+
+  return (
+    <main>
+      {/* <Carousel /> */}
+      <SlickCarousal />
+    </main>
+  );
+};
+export default App;
